ff1575c2a42cd7bdb59a0060392ec2fb688b6759
Redaska <reda.essakali@gmail.com>
2023-11-12 17:55:29 +0100

    Add prototypes

--- main.h
+++ main.h
@@ -7,5 +7,15 @@
 #include <limits.h>
 
 
+/*print function*/
+int _printf(const char *format, ...);
+
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif
