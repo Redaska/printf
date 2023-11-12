8a702cb22273bb1dcb50f4d011f0b1efcbf570f8
Redaska <reda.essakali@gmail.com>
2023-11-12 18:18:11 +0100

    Define Unused - Add flags - Make structure

--- main.h
+++ main.h
@@ -6,6 +6,40 @@
 #include <string.h>
 #include <limits.h>
 
+#define UNUSED(x) (void)(x)
+
+/* FLAGS */
+#define F_MINUS 1 /* - */
+#define F_PLUS 2 /* + */
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
 
 /*print function*/
 int _printf(const char *format, ...);
